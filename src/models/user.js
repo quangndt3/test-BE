84addd310dd11817f0cff58b245dd15cbc0fc9fb
quangdbph28170 <118526359+quangdbph28170@users.noreply.github.com>
2024-01-26 11:45:41 +0700

    update

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,7 @@
 import mongoose from "mongoose";
 const user = new mongoose.Schema({
-    name:{
-        type: String,
-    },
     email:{
         type: String,
-    },
-    images: {
-        type:String,
-        require: true,
     },
     password:{
         type: String,
